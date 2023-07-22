2330a361d181ff94c1266411fbc88bcf02058c5a
abhishek <fragenabhishek@gmail.com>
2023-07-22 11:41:18 +0530

    updated

--- daily_commit.bat
+++ daily_commit.bat
@@ -1,21 +1,32 @@
 @echo off
 
-setlocal
-
 REM Set the path to your Git repository
 set "repo_path=C:\Users\abhishs19\Documents\Next\Daily-Commit"
 
 REM Set the commit message for each auto-commit
 set "commit_message=Daily auto-commit"
 
+:START
+REM Clone or pull the latest version of the script from the repository
+if not exist "script_repo" (
+    git clone https://github.com/fragenabhishek/Daily-Commit.git script_repo 
+) else (
+    cd script_repo
+    git pull
+    cd ..
+)
+
+REM Replace the current script with the updated version
+move /y "script_repo\script.bat" "%~dp0"
+
 REM Change to the repository directory
 cd /d "%repo_path%"
 
-REM Loop to perform commits daily
-:LOOP
-git add --all
+REM Commit and push changes to the repository
+git add .
 git commit -m "%commit_message%"
 git push origin main
-timeout /t 86400 >NUL
-goto :LOOP
 
+REM Wait for 1 minute before the next commit
+timeout /t 60 >NUL
+goto START
